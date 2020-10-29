29a3669bebfe8f0d8ed98ae15117fbc90e3d1f60
michael dilulo <snoleopard01001101@gmail.com>
2020-10-29 10:04:57 -0700

    Convert Minutes into Seconds

--- pwj-challenges/main.js
+++ pwj-challenges/main.js
@@ -6,3 +6,10 @@ function sumOfNumbers(num1, num2) {
 
 console.log("Sum of Two Numbers", sumOfNumbers(2, 5))
 
+// Convert Minutes to Seconds
+
+function convertToSeconds(minutes) {
+    return minutes * 60;
+}
+
+console.log("Convert to Seconds", convertToSeconds(7))
