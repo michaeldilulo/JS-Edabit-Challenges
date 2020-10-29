c8df07c039d2f46ef81292bec5e31b4ba0be37c3
michael dilulo <snoleopard01001101@gmail.com>
2020-10-29 10:25:23 -0700

    Convert Age into Seconds

--- pwj-challenges/main.js
+++ pwj-challenges/main.js
@@ -13,3 +13,21 @@ function convertToSeconds(minutes) {
 }
 
 console.log("Convert to Seconds", convertToSeconds(7))
+
+// Age In Seconds
+
+function ageInSeconds(age) {
+    // input my age
+    // let age = 30
+    // multiply it by 365 to get how many days ive been alive
+    // age * 365 = 730
+    // 60 seconds in a minute
+    // sum of 730 * 24 * 60 * 60 = seconds alive
+    // 60 minutes in an hour 
+    // sum of 730 * 24 * 60
+    // 24 hours in a day
+    // 730 * 24
+    return age * 365 * 24 * 60 * 60;
+}
+
+console.log("Age In Seconds", ageInSeconds(25));
